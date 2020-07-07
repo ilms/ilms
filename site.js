c831e0d823e9eb6183fd6eaa1face5e0333c2e45
ilms <44130796+ilms@users.noreply.github.com>
2020-07-07 03:28:56 -0700

    Temporarily disabled image caching

--- site.js
+++ site.js
@@ -125,6 +125,7 @@ function closeMenu() {
 	}, 1500);
 }
 function updateCache() {
+	return; // TODO remove, temporary disabled cache
 	var cache = document.getElementById("cache");
 	while (cache.firstChild) {
 		cache.removeChild(cache.firstChild);
